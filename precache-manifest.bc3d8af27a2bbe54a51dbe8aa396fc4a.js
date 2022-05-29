24b9a788c0292ac73561fda48b4d3d920a239188
yash-burnwal <byash.iitk@gmail.com>
2022-05-29 14:23:55 +0530

    Updates

---
+++ precache-manifest.bc3d8af27a2bbe54a51dbe8aa396fc4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62303a4a208f7d7bd461",
+    "url": "/product-search/static/css/main.c108173a.chunk.css"
+  },
+  {
+    "revision": "62303a4a208f7d7bd461",
+    "url": "/product-search/static/js/main.62303a4a.chunk.js"
+  },
+  {
+    "revision": "a5ed6b819c25ddd166c5",
+    "url": "/product-search/static/js/1.a5ed6b81.chunk.js"
+  },
+  {
+    "revision": "46339a42eb79ea647df0",
+    "url": "/product-search/static/js/runtime~main.46339a42.js"
+  },
+  {
+    "revision": "f21817d8c50cfb1eee7186954f26005d",
+    "url": "/product-search/index.html"
+  }
+];
